869bc5d9011904f67a50464bf49b02a3d6001f28
aishwaryaaish13 <your.email@example.com>
2025-12-25 22:59:36 +0530

    Dynamic and protected routing using React Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Todos from "./pages/Todos";
+import TodoDetails from "./pages/TodoDetails";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+
+      <Route
+        path="/todos"
+        element={
+          <ProtectedRoute>
+            <Todos />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/todos/:todoId"
+        element={
+          <ProtectedRoute>
+            <TodoDetails />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
   );
 }
 
